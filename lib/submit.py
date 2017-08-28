4aab35fbea10a6cb28a1e9003c7b1d76bd355fd3
NateScarlet <NateScarlet@Gmail.com>
2017-08-28 14:09:14 +0800

    submit 0.1.0 # Add version; remove test statement

--- lib/submit.py
+++ lib/submit.py
@@ -3,9 +3,10 @@
 
 import cgtwb
 
+__version__ = '0.1.0'
+
 if __name__ == '__main__':
     CUR = cgtwb.Current()
     print(CUR.selected_ids, 1)
     CUR.task_module.init_with_id(CUR.selected_ids)
     CUR.submit(CUR.files, note=u'自cgtw提交')
-    input()
