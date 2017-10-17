f4ceab1e891c04c66e3d30d9a6ae9d2280832d3c
NateScarlet <NateScarlet@Gmail.com>
2017-10-17 15:48:24 +0800

    Revert "uploader # Add log for debug"
    This reverts commit 759ecb77ad87859b85ca1aadcf31306f6394b1aa.

--- lib/run_uploader.py
+++ lib/run_uploader.py
@@ -2,10 +2,5 @@
 """Cgteamwork uploader entry. """
 
 from wlf import uploader
-from wlf.mp_logging import set_basic_logger
-import logging
-
-set_basic_logger()
-logging.getLogger().setLevel(logging.DEBUG)
 
 uploader.main()
