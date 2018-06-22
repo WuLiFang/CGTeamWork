ae8514e8df9de8cdb1d535c48f2d7c13d5fbe4f5
NateScarlet <NateScarlet@Gmail.com>
2018-06-22 11:45:19 +0800

    Add prefix filter to uploader.

--- lib/run_uploader.py
+++ lib/run_uploader.py
@@ -3,8 +3,18 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+
+def setup_prefix_filter():
+    """Add custom prefix filter for wrong naming.  """
+
+    import cgtwq.helper.wlf
+    cgtwq.helper.wlf.CGTWQHelper.prefix_filters.append(
+        lambda x: x.replace('XJCG', 'XJ'))
+
+
 if __name__ == '__main__':
     import localsite
     localsite.setup()
+    setup_prefix_filter()
     import cgtwq_uploader
     cgtwq_uploader.main()
