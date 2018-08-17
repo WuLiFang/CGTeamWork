6efd93ab4aff6da367e917e2cbcb44a8e74712f3
NateScarlet <NateScarlet@Gmail.com>
2018-08-17 18:02:50 +0800

    Refactor `filetools`

--- lib/filetools.py
+++ lib/filetools.py
@@ -3,16 +3,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import os
+from wlf.pathtools import make_path_finder
 
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-
-def path(*other):
-    """Get path relative to `ROOT`.
-
-    Returns:
-        str -- Absolute path under root.
-    """
-
-    return os.path.abspath(os.path.join(ROOT, *other))
+path = make_path_finder(__file__)  # pylint: disable=invalid-name
