6c0d6bc18cfb22b98bfbe15a7cf5f33570147d8c
NateScarlet <NateScarlet@Gmail.com>
2018-09-27 18:06:42 +0800

    Refactor assign_downstream

--- lib/assign_downstream.py
+++ lib/assign_downstream.py
@@ -19,32 +19,37 @@ def assign_same_to_downstream(select):
     """Assign downstream task to current artists.  """
 
     assert isinstance(select, cgtwq.Selection)
-    data = select.get_fields('shot.shot', 'account_id', 'pipeline')
-    pipeline = data.column('pipeline')
-    assert len(pipeline) == 1, '选择了多个流程: {}'.format(pipeline)
-    pipeline = pipeline[0]
-    target_select = select.module.filter((cgtwq.Field('shot.shot').in_(data.column('shot.shot')))
-                                         & (cgtwq.Field('pipeline') == DOWNSTREAM_DICT[pipeline]))
-    target_data = target_select.get_fields('id', 'shot.shot', 'account_id', )
+    pipeline = select.pipeline.one()
+    current_data = select.get_fields('shot.shot', 'account_id')
+
+    target = select.module.filter(
+        cgtwq.Field('shot.shot').in_(current_data.column('shot.shot')),
+        cgtwq.Field('pipeline') == DOWNSTREAM_DICT[pipeline.name])
+    target_data = target.get_fields('id', 'shot.shot', 'account_id')
+
     errored_shots = []
-    for shot, account_id, _ in data:
-        account_ids = set(account_id.split(','))
+
+    for shot, account_id in current_data:
         try:
-            target = select.module.select(
-                *[i[0] for i in target_data if i[1] == shot])
+            _target = select.module.select(
+                *[i[0] for i in target_data
+                  if i[1] == shot])
+            assert len(_target) == 1, _target
+            _target_data = (i for i in target_data if i[0] in _target)
         except ValueError:
             errored_shots.append(shot)
             continue
-        assert len(target) == 1, target
 
-        target_account_ids = set(i
-                                 for j in target_data if j[0] in target
-                                 for i in j[2].split(','))
+        account_ids = set(account_id.split(','))
+        target_account_ids = set(j
+                                 for i in _target_data
+                                 for j in i[2].split(','))
+
         if target_account_ids == account_ids:
             print('跳过已设置: {}'.format(shot))
             continue
         print('设置制作者: {}: {} -> {}'.format(shot, target_account_ids, account_ids))
-        target.flow.assign(account_ids)
+        _target.flow.assign(account_ids)
 
     if errored_shots:
         print('以下镜头无输出阶段:')
