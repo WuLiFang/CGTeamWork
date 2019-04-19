def6d18f756cea746b0987e048027b2ad2aba953
NateScarlet <NateScarlet@Gmail.com>
2019-04-19 16:06:36 +0800

    fix: jupyter encoding issue

--- lib/run_jupyter.py
+++ lib/run_jupyter.py
@@ -4,15 +4,18 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import os
 import subprocess
 
 from filetools import path
 
 
 def main():
+    env = dict(os.environ)
+    env[b'LANG'] = b'en_US'
     exit(subprocess.call(
         [path('site-packages/bin/jupyter-notebook')],
-        cwd=path('../notebook')))
+        cwd=path('../notebook'), env=env))
 
 
 if __name__ == '__main__':
