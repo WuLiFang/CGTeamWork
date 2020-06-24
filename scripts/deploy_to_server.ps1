579a497ae99c1d8b93ee912dc64af9732c4cd663
NateScarlet <NateScarlet@Gmail.com>
2020-06-24 14:13:16 +0800

    chore: improve deploy script

--- scripts/deploy_to_server.ps1
+++ scripts/deploy_to_server.ps1
@@ -1,2 +1,12 @@
-git push server
-ROBOCOPY .venv\Lib\site-packages "$(git remote get-url server)\.venv\Lib\site-packages" /MIR /R:0
+git.exe push server
+if (!$?) {
+    exit 1
+}
+git.exe -C $(git remote get-url server) checkout $(git rev-parse HEAD)
+if (!$?) {
+    exit 1
+}
+Robocopy.exe .venv\Lib\site-packages "$(git remote get-url server)\.venv\Lib\site-packages" /MIR /R:0
+if (!$?) {
+    exit 1
+}
