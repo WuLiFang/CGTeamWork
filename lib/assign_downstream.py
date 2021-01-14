d7669de57925cc332cefb6a26b77bf7b6f80c46d
NateScarlet <NateScarlet@Gmail.com>
2021-01-14 11:05:03 +0800

    feat(assign_downstream): support multiple downstream pipeline

--- lib/assign_downstream.py
+++ lib/assign_downstream.py
@@ -3,8 +3,7 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
-import pprint
+import logging
 
 import win_unicode_console
 
@@ -13,6 +12,7 @@ from wlf.console import pause
 
 DOWNSTREAM_DICT = {'灯光': ['渲染', '预渲染'],
                    '合成': ['输出']}
+LOGGER = logging.getLogger(__name__)
 
 
 def assign_same_to_downstream(select):
@@ -25,49 +25,46 @@ def assign_same_to_downstream(select):
     target = select.module.filter(
         cgtwq.Field('shot.shot').in_(current_data.column('shot.shot')),
         cgtwq.Field('pipeline').in_(DOWNSTREAM_DICT[pipeline.name]))
-    target_data = target.get_fields('id', 'shot.shot', 'account_id')
-
-    errored_shots = []
+    target_data = target.get_fields(
+        'id',
+        'shot.shot',
+        'account_id',
+        'pipeline',
+    )
 
     for shot, account_id in current_data:
-        try:
-            _target = select.module.select(
-                *[i[0] for i in target_data
-                  if i[1] == shot])
-            assert len(_target) == 1, _target
-            _target_data = (i for i in target_data if i[0] in _target)
-        except ValueError:
-            errored_shots.append(shot)
-            continue
-
         account_ids = set(account_id.split(','))
-        target_account_ids = set(j
-                                 for i in _target_data
-                                 for j in i[2].split(','))
-
-        if target_account_ids == account_ids:
-            print('跳过已设置: {}'.format(shot))
-            continue
-        print('设置制作者: {}: {} -> {}'.format(shot, target_account_ids, account_ids))
-        _target.flow.assign(account_ids)
-
-    if errored_shots:
-        print('以下镜头无输出阶段:')
-        pprint.pprint(errored_shots)
-    return errored_shots
+        for _id, _, target_account_id, pipeline in (
+                i for i in target_data
+                if i[1] == shot
+        ):
+            target_account_ids = set(target_account_id.split(','))
+            if target_account_ids == account_ids:
+                LOGGER.info('跳过已设置: %s: %s', pipeline, shot)
+                continue
+            LOGGER.info(
+                '设置制作者: %s: %s: %s -> %s',
+                pipeline,
+                shot,
+                target_account_ids,
+                account_ids,
+            )
+            select.module.select(_id).flow.assign(account_ids)
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+
     win_unicode_console.enable()
     client = cgtwq.DesktopClient()
     client.connect()
     select = client.selection()
 
     try:
-        errored_shots = assign_same_to_downstream(select)
-        pause(0 if errored_shots else 5)
+        assign_same_to_downstream(select)
+        pause(5)
     except KeyError:
-        print('当前仅支持流程: {}'.format(','.join(DOWNSTREAM_DICT.keys())))
+        LOGGER.error('当前仅支持流程: {}'.format(','.join(DOWNSTREAM_DICT.keys())))
         pause(0)
     except:
         import traceback
